6a6b9c7eecd178a04fd484ca8c615de026cfb58f
Evan Pearson <parsnbl@icloud.com>
2023-12-07 10:35:47 -0800

    initial split

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const WaveJS = require('../../wavejs/src');
+
+const server = new WaveJS();
+
+server.configureAV({
+  hlsListSize: ['-hls_list_size', '0'],
+  protocols: ['dash'],
+});
+server.setInput({ endpoint: 'wavejs', streamId: 'mvp-demo' });
+server.setOutput({ endpoint: 'wavejs', port: 3000 });
+
+server.listen();
