c9b2f0bca27c05be39a9ab28309761f36b98b665
Evan Pearson <parsnbl@icloud.com>
2023-12-09 11:36:58 -0800

    new control scheme

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const path = require('path');
-const WaveJS = require('@wave.js/wave.js');
+
+//for local WaveJS, uncomment the bellow
+const { WaveJS, Logger } = require('wave.js')
+// for production WaveJS, uncomment the below
+//const WaveJS = require('@wave.js/wave.js');
 
 const app = express();
 
@@ -29,12 +33,17 @@ if (mode === 'production') {
 
 app.use(express.static(path.join(__dirname, '../../dist')));
 
-server.configureAV({ hlsListSize: ['-hls_list_size', '0'] });
-server.setInput({ endpoint: 'wavejs' });
-server.setOutput({ endpoint: 'wavejs', port: 3000 });
+
+//Logger.setLogLevel(5) //debug, default is just info
+
+server.updateMediaDir(path.join(__dirname, 'media'))
+server.updateOutputProtocol('hls')
+server.updateHLSOutput({ hlsListSize: 0 })
+server.updateInputSettings({ endpoint: 'wavejs' });
+server.updateOutputSettings({ endpoint: 'wavejs', port: 3000 });
 
 server.listen();
 
 app.listen(port, () => {
-  console.log('Listening on port ', port);
+  Logger.info('Listening on port ', port);
 });
